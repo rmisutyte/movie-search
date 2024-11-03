b43cab92164e017652763ff20c314cef1fb4635a
rmisutyte <ruta.mi@protonmail.com>
2024-11-03 15:25:54 +0200

    rating

---
+++ src/app/my-list/my-list.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-list.component.html',
+  styleUrl: './my-list.component.scss',
+})
+export class MyListComponent {
+  dummyMovieList = [
+    {
+      title: 'Gaslit',
+      poster:
+        'https://m.media-amazon.com/images/M/MV5BMjgxODBlMmQtNDUxOC00ZTFjLTk2YWUtMTlkZjY5MThhNDQwXkEyXkFqcGc@._V1_SX300.jpg',
+      year: '2022',
+      director: 'Robbie Pickering',
+      genre: 'Biography, Drama, History',
+      type: 'series',
+    },
+    {
+      title: 'Kill Bill: Vol. 1',
+      poster:
+        'https://m.media-amazon.com/images/M/MV5BZmMyYzJlZmYtY2I3NC00NjAyLTkyZWItZjdjZDI1YTYyYTEwXkEyXkFqcGc@._V1_SX300.jpg',
+      year: '2003',
+      director: 'Quentin Tarantino',
+      genre: 'Action, Crime, Thriller',
+      type: 'movie',
+    },
+  ];
+}
